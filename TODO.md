cf771e9a581dd61130ee8ce4e7b0f7bee1d4dc12
Ruben <chickenzrage@gmail.com>
2021-05-24 23:26:15 +0100

    Improve window asthetics/infomation

--- TODO.md
+++ TODO.md
@@ -44,6 +44,13 @@ ei_timings: when ei happens, if IE & IF != 0 already then it takes 4 clock cycle
 - https://sound.stackexchange.com/questions/34816/what-is-a-noise-sweep
 - https://github.com/simias/gb-rs/blob/master/src/ui/sdl2/audio.rs
 - https://www.youtube.com/watch?v=72dI7dB3ZvQ
+- https://github.com/Rust-SDL2/rust-sdl2/blob/master/examples/audio-squarewave.rs
+- https://emudev.de/gameboy-emulator/bleeding-ears-time-to-add-audio/
+
+# Node FFI
+- https://stackoverflow.com/questions/36604010/how-can-i-build-multiple-binaries-with-cargo
+- https://www.reddit.com/r/rust/comments/jg1qm2/electron_rust_how_to_talk_to_each_other/
+- napi-rs
 
 # Misc
 - DMA bus conflicts (ram trashing thing?)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::time::Duration;
+use std::{collections::VecDeque, time::Duration};
 
 use gameboy_rs::gameboy::GameBoy;
-use sdl2::{pixels::PixelFormatEnum, render::{Canvas, Texture}, video::Window};
+use sdl2::{pixels::{PixelFormatEnum}, render::{Canvas, Texture}, surface::Surface, video::Window};
 
 const SCALE: u32 = 2;
 const WIDTH: u32 = 160;
@@ -19,12 +19,60 @@ fn main() {
 
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
-    let window = video_subsystem
-        .window("frosty", WIDTH * SCALE, HEIGHT * SCALE)
+    let mut window = video_subsystem
+        .window("Frosty", WIDTH * SCALE, HEIGHT * SCALE)
         .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let icon_data: [u8; 16*16] = [
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
+    ];
+
+    let mut real_icon_data = [0u8; 16 * 16 * 3];
+    let mut i = 0usize;
+    while i < 16 * 16 * 3 {
+        let index = i / 3;
+        let color = icon_data[index];
+
+        if color != 0 {
+            real_icon_data[i] = 0;
+            real_icon_data[i + 1] = 162;
+            real_icon_data[i + 2] = 232;
+        } else {
+            real_icon_data[i] = !color;
+            real_icon_data[i + 1] = !color;
+            real_icon_data[i + 2] = !color;
+        }
+
+        i += 3;
+    }
+
+    let icon = Surface::from_data(
+        &mut real_icon_data, 
+        16, 
+        16, 
+        16 * 3, 
+        PixelFormatEnum::RGB24
+    ).unwrap();
+
+    window.set_icon(icon);
 
     let mut canvas = window
         .into_canvas()
@@ -38,9 +86,12 @@ fn main() {
         .map_err(|e| e.to_string())
         .unwrap();
 
-    let mut frame_count: u64 = 0;
     let timer = sdl.timer().unwrap();
     let mut turbo = false;
+
+    let mut elapsed_ms: u64 = 0;
+    const FRAMERATE_UPDATE_MS: u64 = 1000000;
+    let mut comutative_speed: VecDeque<f64> = VecDeque::new();
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -57,7 +108,10 @@ fn main() {
                     if !repeat && keycode.is_some() {
                         let keycode = keycode.unwrap();
                         match keycode {
-                            sdl2::keyboard::Keycode::Tab => turbo = true,
+                            sdl2::keyboard::Keycode::Tab => {
+                                turbo = true;
+                                comutative_speed.clear();
+                            },
                             _ => gb.key_down(keycode)
                         }
                     }
@@ -74,7 +128,10 @@ fn main() {
                    if !repeat && keycode.is_some() {
                        let keycode = keycode.unwrap();
                        match keycode {
-                        sdl2::keyboard::Keycode::Tab => turbo = false,
+                        sdl2::keyboard::Keycode::Tab => {
+                            turbo = false;
+                            comutative_speed.clear();
+                        },
                         _ => gb.key_up(keycode)
                     }
                    }
@@ -92,25 +149,38 @@ fn main() {
         }
 
         update_canvas(&mut texture, &mut canvas, &gb);
-        frame_count += 1;
 
         let end = timer.performance_counter();
         let elapsed = (end - start) as f64 / timer.performance_frequency() as f64 * 1000.0;
 
-        if !turbo && elapsed < 16.6666 {
-            let sleep_amount = (16.6666 - elapsed) as u64;
+        if !turbo && elapsed < 16.75041 {
+            let sleep_amount = (16.75041 - elapsed) as u64;
             std::thread::sleep(Duration::from_millis(sleep_amount));
         }
 
         let end = timer.performance_counter();
 
-        if frame_count % 180 == 0 {
+        elapsed_ms += end - start;
+
+        if elapsed_ms > FRAMERATE_UPDATE_MS {
+            elapsed_ms -= FRAMERATE_UPDATE_MS;
             let elapsed = (end - start) as f64 / timer.performance_frequency() as f64;
-            canvas.window_mut().set_title(format!("frosty fps: {:.1}", 1.0f64 / elapsed).as_str()).unwrap()
+            let fps = 1.0f64 / elapsed;
+            let percent = (fps / 59.7) * 100.0;
+
+            comutative_speed.push_back(percent);
+            if comutative_speed.len() > 100 {
+                comutative_speed.pop_front();
+            }
+
+            let average_percent = comutative_speed.iter().sum::<f64>() / comutative_speed.len() as f64;
+
+            canvas.window_mut().set_title(format!("Frosty  [{:.1}%]", average_percent).as_str()).unwrap()
         }
     }
 }
 
+#[inline]
 fn update_canvas(texture: &mut Texture, canvas: &mut Canvas<Window>, gb: &GameBoy) {
     let frame_buffer = gb.get_frame_buffer();
 
